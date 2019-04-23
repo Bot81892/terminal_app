97401b2ad3c0e97d8f82318afaa38d1ba629deef
Melissa <Melissamcadam24@gmail.com>
2019-04-23 13:32:39 +1000

    able to choose class and attack with each class

--- terminal_app.rb
+++ terminal_app.rb
@@ -18,11 +18,33 @@ class Character
     def gets_hit(damage)
         @health -= damage
     end
-    def jab()
-        print "Player jabs! #{@name} deals #{@attk_damage} damage\n"
-    end
 
 end
+
+class Fighter < Character
+    attr_accessor(:name, :attk_damage, :health, :attacks)
+    def initialize(name, attk_damage)
+        super(name, attk_damage)
+    end
+    def jab()
+        "Player jabs! #{@name} deals #{@attk_damage} damage\n"
+    end
+    def SpitefulBugWhirlingFish()
+        "Player uses spiteful bug whirling fish! #{@name} deals #{@attk_damage} damage\n"
+    end
+    @attacks = ["jab", "spiteful bug whirling fish"]
+end
+
+class Mage < Character
+    attr_accessor(:name, :attk_damage, :health)
+    def initialize(name, attk_damage)
+        super(name, attk_damage)
+    end
+    def fireball()
+        "Player attacks with a firey blast! #{@name} deals #{@attk_damage} damage\n"
+    end
+end
+
 
 class Skeleton
     attr_accessor(:name, :attk_damage, :health)
@@ -50,47 +72,53 @@ class Skeleton
 end
 
 
-
-# class Skeleton < Character
-#     attr_accessor(:name, :attk_damage, :health)
-#     def initialize(name, attk_damage)
-#         super(name, attk_damage)
-#     end
-#     def slash(character_health)
-#         print "Slash! #{@name} deals #{@attk_damage} damage\n"
-#         character_health -= @attk_damage
-#     end
-
-# end
-
-fighter = Character.new("Fighter", 20)
-# mage = Character.new("Mage", 20)
 skeleton = Skeleton.new("Skeleton", 15)
 
-# fighter.to_s
-# skeleton.slash(fighter.health)
-# fighter.gets_hit(skeleton.attk_damage)
-# fighter.to_s
+mage = Mage.new("Mage", 20)
 
-# mage.to_s
+print "Welcome to The Reclaimer\n"
 
-# print "#{fighter}\n"
-# print "#{mage}\n"
+print "----story----\n"
 
-while (true)
+print "Please select your class: "
+class_name = gets.strip()
+print "---------------------------\n"
 
-    fighter.jab
-    skeleton.gets_hit(fighter.attk_damage)
-  
-    skeleton.slash
-  fighter.gets_hit(skeleton.attk_damage)
-    
-    if (fighter.health <= 0)
-        print "Player has died!\n"
-        break
+if (class_name == "Fighter")
+    fighter = Fighter.new("Fighter", 20)
+    player_type = fighter
+    player_attk = fighter.jab
+
+elsif (class_name == "Mage")
+    mage = Mage.new("Mage", 20)
+    player_type = mage
+    player_attk = player_type.fireball
+end
+
+
+
+battling = true
+while (battling)
+    print "What attack would you like to use?"
+    print "Available attacks are: "
+    (player_type.attacks).each do |attack|
+        print attack
     end
+    printplayer_attk
+    
+    skeleton.gets_hit(player_type.attk_damage)
+    print "Skeletons health is #{skeleton.health}\n"
     if (skeleton.health <= 0)
         print "You are victorious!\n"
         break
     end
+    print "-----------------------------\n"
+    skeleton.slash
+    player_type.gets_hit(skeleton.attk_damage)
+    print "Players health is #{player_type.health}\n"
+    if (player_type.health <= 0)
+        print "Player has died!\n"
+        break
+    end
+    print "-----------------------------\n"
 end
