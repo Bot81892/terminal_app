82295c6a40521493975b48d3dca303b000e5dddd
Melissa <Melissamcadam24@gmail.com>
2019-04-24 11:17:33 +1000

    added easter egg and README and debugged.

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# terminal_app
+# Terminal_App "The Reclaimer"
+
+This application is a game, were you can chose a class, and deal damage to monsters and with an engaging story line. It is run in the Bash command line written in ruby.
+
+#Contributors
+
+Jovan Nikolic and Melissa McAdam
+
+#Link
+
+https://github.com/Bot81892/terminal_app
+
+#Purpose
+
+Made for gamers that want a fun way to waste time and love a simpleifed old school style game. The Reclaimer is a story driven game made to emerse the user within it's world and the challenges they must face, the goal is for the player to overcome challenges and find intrig with thee story line, resulting in a fun experience.
+
+#Functionality
+
+ At the beging like most games is the narative story for the user to be introduced into The Reclaimers world. The game will promt the user to input a name and then choose a caracter class to play as. <!-- Based on user inputs, The Reclaimer will return a class selection, enter the arena and attack the enemy with the users choice of attack. The user can exit the game at any time-->
+
+ #Instructions for use
+
+Open Terminal or preferred console. To initialise program, enter:
+
+Gem install
+
+<!--ascii art gem 
+    colorize gem 
+    font??-->
+
+$ ruby terminal_app.rb
+<!--ReName the file to "The Reclaimer"?-->
+Ready to go!
+<!-- add a screen shoot of the main Menu-->
+
+From the main menu, make a selection: 
+
+ # Design & Planing
+
+
+<!-- Project documentation is to be compiled as a single markdown file named README.md
+
+This file should contain,
+
+A link to your GitHub repository
+Ensure the repository (repo) is accessible by your Educators
+Description of the app, including,
+Purpose
+Functionality
+Instructions for use
+Screenshots
+Details of design & planning process including,
+Evidence of app idea brainstorming sessions
+app User/Workflow diagram
+Project plan & timeline
+Screenshots of Trello board(s) -->
--- terminal_app.rb
+++ terminal_app.rb
@@ -4,6 +4,7 @@ class Character
         @name = name
         @health = 100
         @attk_damage = attk_damage
+        # @items = []
     end
     def to_s()
         print "The name of the class is #{@name}, attk damage: #{@attk_damage}, health: #{@health}\n"
@@ -32,12 +33,6 @@ class Attack
         print "Player uses #{@name}! #{character} deals #{@damage_amount} damage\n"
     end
 end
-
-jab = Attack.new("jab", 20)
-sbwf = Attack.new("spiteful bug whirling fish", 10)
-
-fireball = Attack.new("fireball", 20)
-shadow_volley = Attack.new("shadow volley", 10)
 #-------------------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------------------
@@ -46,7 +41,7 @@ class Fighter < Character
     attr_accessor(:name, :attk_damage, :health, :attacks)
     def initialize(name, attk_damage)
         super(name, attk_damage)
-        @attacks = []
+        @attacks = [Attack.new("jab", 20), Attack.new("spiteful bug whirling fish", 10)]
     end
     def add_attack(attack)
         @attacks << attack
@@ -57,7 +52,7 @@ class Mage < Character
     attr_accessor(:name, :attk_damage, :health, :attacks)
     def initialize(name, attk_damage)
         super(name, attk_damage)
-        @attacks = []
+        @attacks = [Attack.new("fireball", 20), shadow_volley = Attack.new("shadow volley", 10)]
     end
     def add_attack(attack)
         @attacks << attack
@@ -65,17 +60,15 @@ class Mage < Character
 end
 #------------------------------------------------------------------------------------
 
-
-
-
 #------------------------------------------------------------------------------------
 #Enemy class
 class Skeleton
     attr_accessor(:name, :attk_damage, :health)
-    def initialize(name, attk_damage)
+    def initialize(name, attk_damage, health)
         @name = name
-        @health = 100
+        @health = health
         @attk_damage = attk_damage
+        # @item_drop = item
     end
     def to_s()
         print "The name of the class is #{@name}, attk damage: #{@attk_damage}, health: #{@health}\n"
@@ -93,62 +86,136 @@ class Skeleton
         print "Slash! #{@name} deals #{@attk_damage} damage\n"
     end
 end
+#------------------------------------------------------------------------------
 
+#------------------------------------------------------------------------------
+#METHODS used in the program
 
-skeleton = Skeleton.new("Skeleton", 15)
-draugr = Skeleton.new("Draugr", 25)
-
-
-
-print "Welcome to The Reclaimer\n"
-print "----story----\n"
-
-print "Please select your class: "
-class_name = gets.strip()
-print "---------------------------\n"
-
-if (class_name == "Fighter")
-    fighter = Fighter.new("Fighter", 20)
-    fighter.add_attack(jab)
-    fighter.add_attack(sbwf)
-    player_type = fighter
-elsif (class_name == "Mage")
-    mage = Mage.new("Mage", 20)
-    mage.add_attack(fireball)
-    mage.add_attack(shadow_volley)
-    player_type = mage
+def load_dots()
+    1.upto(3) do |n|
+        print "."
+        sleep 1
+    end
 end
 
+def intro()
+    print "Welcome to The Reclaimer"
+    load_dots()
+    print "\n----story----\n"
+end
 
+def loadmove(time)
+    time.times do
+        sleep 1
+    end
+end
+
+def class_select()
+    print "Please select your class: "
+    class_name = gets.strip()
+    selecting_class = true
+    while (selecting_class)
+        if (class_name == "Fighter")
+            return Fighter.new("Fighter", 20)
+        elsif (class_name == "Mage")
+            return Mage.new("Mage", 20)
+        else 
+            print "Sorry, that is not a proper class\n"
+        end
+        #At this point a proper class has not been selected, so we prompt the user again
+        print "Please select your class: "
+        class_name = gets.strip
+    end
+end
+
+def spawn_enemies()
+    return [Skeleton.new("Skeleton", 15, 100), Skeleton.new("Draugr", 20, 150)]
+end
+
+def easter_egg_dialogue()
+    print "You: do we need to keep fighting?\n"
+    loadmove(2)
+    print "Enemy: i don't know, it seems pointless\n"
+    loadmove(2)
+    print "You: i think so too, can i just have my kingdom back?\n"
+    loadmove(2)
+    print "Enemy: you know what, sure, we dont want this kingdom anymore, its yours\n"
+    loadmove(4)
+    print "CONGRATULATIONS, YOU HAVE RECLAIMED YOUR KINGDOM THE PEACEFUL WAY, YOU WIN\n"
+    exit!
+end
+
+def select_attack()
+    print "\nWhat attack would you like to use? "
+    attack = gets.strip
+    selecting_attack = true
+    while (selecting_attack)
+        if (attack == "make peace")
+            easter_egg_dialogue()
+        elsif ((attack.to_i)-1 >= 0 && (attack.to_i-1) < 2)
+            return attack.to_i
+        else
+            print "Sorry, you have not selected an appropriate attack, please try again\n"
+        end
+        #at this point a proper attack has not been selected
+        print "Please select an attack: "
+        attack = gets.strip
+    end
+end
+
+#------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------
+#MAIN 
+
+intro
+player_type = class_select()
+
+# print "#{player_type}\n"
+
+enemies = spawn_enemies()
+enemy = 0
 
 
 battling = true
 while (battling)
-    enemy = 0
+    
     #---------------------------------------------------------
     #Player attacks
     print "Available attacks are: "
     (player_type.attacks).each_with_index do |attack, index|
         print "#{index+1}.#{attack.name} "
     end
-    print "\nWhat attack would you like to use? "
-    attack = gets.strip.to_i
+    
+    attack = select_attack()
 
     player_type.attacks[attack-1].list_attack(player_type.name)
 
-    skeleton.gets_hit(player_type.attacks[attack-1].damage_amount)
-    print "Skeletons health is #{skeleton.health}\n"
-    if (skeleton.health <= 0)
+    enemies[enemy].gets_hit(player_type.attacks[attack-1].damage_amount)
+    print "#{enemies[enemy].name}'s health is #{enemies[enemy].health}\n"
+    if (enemies[enemy].health <= 0)
         print "You are victorious!\n"
-        break
+        #--------------------------
+        #Heal
+        print "You have absorbed the enemies spirit and regained health!\n"
+        player_type.update_health(100)
+        print "You are entering a new battle!\n"
+        print "Players health is #{player_type.health}\n"
+        #--------------------------
+        if (enemy == 1)
+            print "You have reclaimed the kingdom!\n"
+            break
+        end
+        enemy += 1
     end
     
     print "-----------------------------\n"
 
     #-----------------------------------------------------
     #Skeleton attacks
-    skeleton.slash
-    player_type.gets_hit(skeleton.attk_damage)
+    enemies[enemy].slash
+    player_type.gets_hit(enemies[enemy].attk_damage)
     print "Players health is #{player_type.health}\n"
     if (player_type.health <= 0)
         print "Player has died!\n"
